a9b22790ff71d2ddf330d3b865a9913d1ccd2e01
John Burk <john.burk98@gmail.com>
2011-06-28 22:12:08 -0400

    added phyutil package and 1-d motion sumulation.py

---
+++ 1-dMotionSimulation.py
@@ -0,0 +1,69 @@
+from __future__ import division
+from visual import *
+from physutil import *
+from visual.graph import *
+
+###SETUP
+
+# Set window title
+scene.title = "1-D motion simulation"
+
+# Define scene objects
+field = box(pos =vector(0,0,0),size=(300,10,100),color = color.green,opacity = 0.3)
+ball = sphere(pos = vector(-150,0,0), radius=5, color = color.blue)
+
+# Set up graph and onscreen curve
+graph = PhysGraph()
+trail = curve(color = color.yellow, radius = 1)
+
+# Define axis that marks the field (divide into 15 equal intervals)
+#IMPORTANT Question: Why do you need 16 Markers to divide the field into 15 intervals? 
+axis = PhysAxis(field, 16)
+
+
+# Define physics parameters
+mball=0.6 #mass of ball
+vball = vector(30,0,0) #initial velocity of ball in (vx,vy,vz) form
+
+# Define time parameters
+t=0 #starting time
+deltat = 0.001  #time step
+
+# Set timer in top right of screen
+timerDisplay = PhysTimer(150,150)
+
+#calculate time for object to cross the field (think carfully about assumptions here)
+tf=field.size.x/vball.x
+
+# Set up MotionMap to display breadcrumbs 
+motionMap = MotionMap(ball, tf, 10, markerType="breadcrumbs",    #drop 10 breadcrumbs between t=0 and tf
+                      labelMarkerOffset=vector(0,-20,0),         #put lables below the marker
+                      dropTime=True, timeOffset=vector(0,35,0)) # put times above the marker
+
+# Set up MotionMap to display velocity vectors
+velocityMap = MotionMap(ball, tf,10, markerScale = 0.5,labelMarkerOrder = false)
+
+###END OF SETUP
+
+# MAIN UPDATE LOOP; perform physics updates and drawing
+while ball.pos.x < 150:  #while the ball's x-position is less than 150
+    # Required to make animation visible / refresh smoothly (keeps program from running faster than 1000 frames/s)
+    rate(1000)    
+
+    # Ball physics update
+    ball.pos = ball.pos + vball*deltat
+
+    # Update motion map, graph plot, onscreen trail
+    motionMap.update(t)
+    velocityMap.update(t, vball)
+    
+    graph.plot(t,ball.pos.x)  #this plots one point in the graph in (x,y) form
+    trail.append(pos = ball.pos)
+
+    # Timer update
+    t = t + deltat
+    timerDisplay.update(t)
+    
+
+print t
+print ball.pos
